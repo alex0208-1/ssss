b5f529090f0aca8c89970ecfd17d85e67bf527f9
佳祐 <you@example.com>
2022-01-24 17:14:57 +0800

    feat(add):finish

---
+++ core/production_settings.py
@@ -0,0 +1,11 @@
+import dj_database_url
+
+from .settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['coffee-shopp.herokuapp.com']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True),
+}
